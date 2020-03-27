7d35b328a756be56a39d9fd2c1dc17700395ea5c
h3m <lanjingu@outlook.com>
2020-03-27 23:02:57 +0800

    完善UI

--- data.py
+++ data.py
@@ -1,8 +1,6 @@
+# -*- coding: utf-8 -*-
 """@ package data
 YAML description:
-
-topo:
-  - name: singleTopo
 host:
 - name: h101
   ip: 10.0.1.1
@@ -22,62 +20,100 @@ link:
     destination: s1
 """
 
-from topo import Topo,Node
+
 import yaml
 import os
+from mininet.topo import Topo, SingleSwitchTopo
+from mininet.net import Mininet
+from mininet.cli import CLI
+from mininet.log import setLogLevel,output,info,debug
+from topo import Nets
 
-def getTopo(fileName):
+class DataManager:
+  def __init__(self,nets):
+        self.nets = nets
+  
+  def getNet(self,fileName):
+    # load data from file
     if not(os.path.exists(fileName) and os.path.isfile(fileName)):
         return False
-    file = open(fileName,'r',encoding="utf-8")
+    file = open(fileName,'r')
     fileData = file.read()
     file.close()
     data = yaml.load(fileData,Loader=yaml.FullLoader)
-    # get topo
-    #TODO： 一个 yaml 文件应该只有一个topo
-    topoName = data.get('topo')
-    if topoName == None:
-        print("Topology does not exist")
-        return False
-    topo = Topo(topoName[0].get('name'))
-
-    # get node
-    type=['switch','host','controller']
-    for t in type:
-        nodes = data.get(t)
-        if nodes != None:
-            for node in nodes:
-                topo.addNode(type=t,name=node.get('name'),ip=node.get('ip'))
     
-    # get link
-    links = data.get('link')
-    if links != None:
-        for l in links:
-            topo.addLink(l.get('source'),l.get('destination'))
+    # get net name
+    netName = data['net']['name']
+    net = self.nets.addNet(netName)
+    
+    # get hosts
+    nodes = data['hosts']
+    for n in nodes:
+          print(n['ip'])
+          self.nets.addHost(n['name'],netName,n['ip'])
+    
+    #get switchs
+    nodes = data['switches']
+    for n in nodes:
+          self.nets.addSwitch(n['name'],netName)
+    
+    #get links
+    links = data['link']
+    for l in links:
+          self.nets.addLink(l['source'],l['destination'],netName)
+    net.start()
+    CLI(net)
+          
+          
 
-    topo.printTopo()
-    return topo
-
-def writeTopo(fileName,topo):
-    if topo == None:
-        return False
+  def writeNet(self,fileName,netname):
     # 一个 yaml 文件只写一个 topology
-    if os.path.exists(fileName):
-        return False
-    #file = open(fileName,'w',encoding="utf-8")
-    data = topo.topoToDict()
-    print(data)
+    # if os.path.exists(fileName):
+    #     return False
+    types = ['hosts','switches','controllers']
+    data = {}
+    data['net'] = {'name': netname}
+
+    #get net from nets
+    net = self.nets.nets.get(netname)
+    if net == None:
+      return False
+
+    for t in types:
+      node_list = []
+      nodes = getattr( net, t)
+      for n in nodes:
+          dic = {}
+          dic['name'] = n.name
+          if n.params.get('mac') != None:
+            dic['mac'] = n.params.get('mac')
+          if n.params.get('ip') != None:
+            dic['ip'] = n.params.get('ip')
+          node_list.append(dic)
+      data[t] = node_list
+    link = getattr(net,'links')
+    link_list = []
+    for l in link:
+          node_dic = {}
+          node_dic['source'] = l.intf1.node.name
+          node_dic['destination'] = l.intf2.node.name
+          link_list.append(node_dic)
+    data['link'] = link_list
     curpath = os.path.dirname(os.path.realpath(__file__))
     yamlpath = os.path.join(curpath, fileName)
-    with open(yamlpath, "w", encoding="utf-8") as f:
+    with open(yamlpath, "w") as f:
         yaml.dump(data, f)
     
 
     
 
     
-
-
 if __name__ == "__main__":
-    topo = getTopo("test.yml")
-    writeTopo("writeTest.yml",topo)
+    setLogLevel('info')
+    nets = Nets()
+    #topo = SingleSwitchTopo()
+    #net = Mininet(topo)
+    #nets.addNet("singleTopo",net)
+    #data = Data(nets)
+    #data.writeNet("singleTopo.yml","singleTopo")
+    #data.getNet("singleTopo.yml")
--- topo.py
+++ topo.py
@@ -1,166 +1,105 @@
+# -*- coding: utf-8 -*-
 """@ package topo
 This package includes code to represent network topologies.
 """
 
-"return False 错误提示写在用户界面"
+from mininet.topo import Topo
+from mininet.net import Mininet
+from mininet.log import setLogLevel,output,info,debug
+from mininet.cli import CLI
+from mininet.node import Controller, RemoteController, Node
 
-class Topologies(object):
+import os
+ 
+class Nets(object):
+    ""
     def __init__(self):
-        self.topos = {}
-        self.topoNum = 0
+        self.nets = {}
+        self.netNum = 0
+        # ruing net
+        self.netName = ""
+        self.net = None
 
-    def addTopo(self, topo=None,topoName=None):
-        if topo:
-            self.topos[topo.name] = topo
-            self.topoNum += 1
-            return True
-        if topoName:
-            topo = Topo(topoName)
-            self.topos[topoName] = topo
-            self.topoNum += 1
-            return True
-        return False
+    def addNet(self, netName, net=None):
+        if net != None:
+            self.nets[netName] = net
+            return net
+        
+        if netName == "":
+            netName = "net" + str(self.netNum)
+        net = Mininet(topo=None, autoSetMacs=True,autoStaticArp=False)
+        # Add a default controller
+        info('*** Adding controller\n' )
+        classes = net.controller
+        if not isinstance( classes, list ):
+            classes = [ classes ]
+        for i, cls in enumerate( classes ):
+            # Allow Controller objects because nobody understands partial()
+            if isinstance( cls, Controller ):
+                net.addController( cls )
+            else:
+                net.addController( 'c%d' % i, cls )
+        self.nets[netName] = net
+        return net
+
+    def runingNet(self, netName):
+        if netName == self.netName:
+            debug("该网络正在运行\n")
+            return False
+        net = self.nets.get(netName)
+        if net == None:
+            debug("net 不存在")
+            return False
+        if self.net != None:
+            debug("停止正在运行的网络:",self.netName)
+            self.net.stop()
+        net.start()
+        self.netName = netName
+        self.net = net
     
-    def deleteTopo(self,name):
-        if name == None:
+    def addHost(self,nodeName,netName=None,IP=None):
+        if netName == None:
+            netName = self.netName
+        net = self.nets.get(netName)
+        if net == None:
+            debug("net 不存在")
             return False
-        if not name in self.topos:
+        net.addHost(nodeName,ip=IP)
+
+    def addSwitch(self,nodeName,netName=None,):
+        if netName == None:
+            netName = self.netName
+        net = self.nets.get(netName)
+        if net == None:
+            debug("net 不存在")
             return False
-        del self.topos[name]
-        self.topoNum -= 1
-        return True
+        net.addSwitch(nodeName)
+    
+    def addLink(self,node1,node2,netName=None):
+        if netName == None:
+            netName = self.netName
+        net = self.nets.get(netName)
+        if net == None:
+            debug("net 不存在")
+            return False
+        net.addLink(node1,node2)
 
-
-"TODO: 添加 Mininet API"
-class Topo(object):
-        def __init__(self,topoName):
-            self.topoName = topoName
-            self.nodes = {}
-            self.link = {}
-            self.count = {}
-            self.count.setdefault("host",0)
-            self.count.setdefault("switch",0)
-            self.count.setdefault("controller",0)
-        
-        def addNode(self,node=None,type=None,name=None,ip=None):
-            if node == None:
-                node = Node(type,name,ip)
-            if node.type == None:
-                node.type = "host"
-            # default name
-            if node.name == None:
-                node.name = node.type + str(self.count[node.type])
-            self.count[node.type] += 1
-            self.nodes[node.name] = node
-            self.link.setdefault(node,[])
-        
-        def deleteNode(self,name):
-            if name == None:
-                return False
-            if not name in self.nodes:
-                return False
-            node = self.nodes[name]
-            self.count[node.type] -= 1
-            del self.nodes[name]
-            return True
-        
-        "TODO: 区分失败的类型"
-        def addLink(self,srcName,destName):
-            if(srcName == None) or (destName == None):
-                return False
-            if not(srcName in self.nodes) or not(destName in self.nodes):
-                return False
-            "sorted by node name"
-            srcName,destName = sorted([srcName,destName])
-            srcNode = self.nodes[srcName]
-            destNode = self.nodes[destName]
-            if self.link[srcNode].count(destNode):
-                print("link already exists")
-                return False
-            self.link[srcNode].append(destNode)
-            return True
-
-        def deleteLink(self, srcName, destName):
-            if(srcName == None) or (destName == None):
-                    return False
-            if not(srcName in self.nodes) or not(destName in self.nodes):
-                return False
-            "sorted by node name"
-            srcName,destName = sorted([srcName,destName])
-            srcNode = self.nodes[srcName]
-            destNode = self.nodes[destName]
-            if self.link[srcNode].count(destNode) == 0:
-                print("link does not exist")
-                return False
-            self.link[srcNode].remove(destNode)
-        
-        # topo 转成字典，方便写入文件
-        def topoToDict(self):
-            data = {}
-            data.setdefault('topo',[])
-            data.setdefault('host',[])
-            data.setdefault('switch',[])
-            data.setdefault('controller',[])
-            data.setdefault('link',[])
-            topoName = {}
-            topoName['name'] = self.topoName
-            topoList = []
-            topoList.append(topoName)
-            nodeList = {}
-            nodeList.setdefault('host',[])
-            nodeList.setdefault('switch',[])
-            nodeList.setdefault('controller',[])
-            #key: node name, value: node
-            for name,node in self.nodes.items():
-                n = {}
-                n['name'] = name
-                n['ip'] = node.ip
-                nodeList[node.type].append(n)
-            # key: srcNode, value： desNode list
-            linkList = []
-            for src,dest in self.link.items():
-                for d in dest:
-                    linkDic = {}
-                    linkDic.setdefault('source',src.name)
-                    linkDic.setdefault('destination',d.name)
-                    linkList.append(linkDic)
-            data['topo'] = topoList
-            data['host'] = nodeList['host']
-            data['switch'] = nodeList['switch']
-            data['controller'] = nodeList['controller']
-            data['link'] = linkList
-            return data
-
-            
-
-        def printTopo(self):
-            topo = self
-            print("topo name:",topo.topoName)
-            print("node list:",end="")
-            for key in topo.nodes:
-                print(topo.nodes[key].name,end=" ")
-            print("\nlink list:")
-            for key in topo.link:
-                print("src：",key.name,end=" link ")
-                for n in topo.link[key]:
-                    print(n.name,end=" ")
-                print("")
-            
+    def getHostInfo(self,hostName):
+        host = self.net.getNodeByName(hostName)
+        info(host.name,host.intfs,host.params)
+        return host.params
 
         
-class Node(object):
-    def __init__(self, type=None,name=None, ip=None):
-        self.name = name
-        self.type = type
-        self.ip = ip
 
+  
 if __name__ == "__main__":
-    topos = Topologies()
-    topos.addTopo(topoName="simpleTopo")
-    topo = topos.topos["simpleTopo"]
-    topo.addNode(type="host",ip="127.0.0.1")
-    topo.addNode(type="host",ip="127.0.0.1")
-    topo.addLink("host0","host1")
-    topo.topoToDict()
-
-    
+    setLogLevel('info')
+    net = Nets()
+    net.addNet("net0")
+    net.addHost("h1","net0",IP = "10.0.0.1")
+    net.addHost("h2","net0",IP = "10.0.0.2")
+    net.addSwitch("s1","net0")
+    net.addLink("s1","h1","net0")
+    net.addLink("s1","h2","net0")
+    net.runingNet("net0")
+    CLI(net.nets["net0"])
